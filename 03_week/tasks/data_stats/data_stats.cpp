4ce36e3061a684d1e5f888c24bdb10899d25a7ce
Ruslan <ruslan@linux-mint.dlink>
2025-12-17 16:00:54 +0500

    add (solution): add data_stats task

--- 03_week/tasks/data_stats/data_stats.cpp
+++ 03_week/tasks/data_stats/data_stats.cpp
@@ -1,11 +1,29 @@
-#include <stdexcept>
-
+#include <vector>
+#include <cmath>
 
 struct DataStats {
     double avg = 0.0;
     double sd = 0.0;
 };
 
-/* return_type */ CalculateDataStats(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+DataStats CalculateDataStats(const std::vector<int>& values) {
+    DataStats data;
+
+    if (values.empty()) {
+        return data;
+    }
+
+    for (const auto& value : values) {
+        data.avg += value;
+    }
+
+    data.avg /= values.size();
+
+    for (const auto& value : values) {
+        data.sd += std::pow(value - data.avg, 2);
+    }
+
+    data.sd = std::sqrt(data.sd / values.size());
+
+    return data;
 }
