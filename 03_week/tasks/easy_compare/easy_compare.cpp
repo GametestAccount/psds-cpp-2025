eab83c3a776c838939732be08b1b6e265a9291e4
Ruslan <ruslan@linux-mint.dlink>
2025-12-17 16:05:00 +0500

    add (solution): add easy_compare task

--- 03_week/tasks/easy_compare/easy_compare.cpp
+++ 03_week/tasks/easy_compare/easy_compare.cpp
@@ -1,10 +1,10 @@
-#include <stdexcept>
-
+#include <cstddef>
+#include <tuple>
 
 struct Date {
-    unsigned year;
-    unsigned month;
-    unsigned day;
+    unsigned year = 0;
+    unsigned month = 0;
+    unsigned day = 0;
 };
 
 struct StudentInfo {
@@ -14,3 +14,35 @@ struct StudentInfo {
     unsigned course;
     Date birth_date;
 };
+
+bool operator==(const Date& lhs, const Date& rhs) {
+    return std::tie(lhs.year, lhs.month, lhs.day) == std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+bool operator<(const Date& lhs, const Date& rhs) {
+    return std::tie(lhs.year, lhs.month, lhs.day) < std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+bool operator!=(const Date& lhs, const Date& rhs) {
+    return !(lhs == rhs);
+}
+
+bool operator>(const Date& lhs, const Date& rhs) {
+    return !((lhs < rhs) || (lhs == rhs));
+}
+
+bool operator<=(const Date& lhs, const Date& rhs) {
+    return !(lhs > rhs);
+}
+
+bool operator>=(const Date& lhs, const Date& rhs) {
+    return !(lhs < rhs);
+}
+
+bool operator==(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return std::tie(lhs.score, lhs.mark) == std::tie(rhs.score, rhs.mark);
+}
+
+bool operator<(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return std::tie(rhs.mark, lhs.score, rhs.course, lhs.birth_date) < std::tie(lhs.mark, rhs.score, lhs.course, rhs.birth_date);
+}
