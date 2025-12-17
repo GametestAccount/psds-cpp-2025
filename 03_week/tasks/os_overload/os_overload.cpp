cb041fcfe26b39692a83ec51c262497ea449e981
Ruslan <ruslan@linux-mint.dlink>
2025-12-17 16:08:23 +0500

    add (solution): add os_overload task

--- 03_week/tasks/os_overload/os_overload.cpp
+++ 03_week/tasks/os_overload/os_overload.cpp
@@ -1,21 +1,55 @@
-#include <stdexcept>
+#include <iostream>
 #include <vector>
 #include <utility>
 
-
 struct Coord2D {
-    int x;
-    int y;
+    int x = 0;
+    int y = 0;
 };
 
 struct Circle {
     Coord2D coord;
-    unsigned radius;
+    unsigned radius = 1;
 };
 
 using CircleRegion = std::pair<Circle, bool>;
 using CircleRegionList = std::vector<CircleRegion>;
 
-/* return_type */ operator<<(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+std::ostream& operator<<(std::ostream& os, const Coord2D& coord) {
+    return os << '(' << coord.x << ", " << coord.y << ')';
+}
+
+std::ostream& operator<<(std::ostream& os, const Circle& circ) {
+    if (circ.radius == 0) {
+        return os << "circle[]";
+    }
+
+    return os << "circle[" << circ.coord << ", r = " << circ.radius << "]";
+}
+
+std::ostream& operator<<(std::ostream& os, const CircleRegion& reg) {
+    os << ((reg.second) ? '+' : '-');
+    return os << reg.first;
+}
+
+std::ostream& operator<<(std::ostream& os, const CircleRegionList& list) {
+    if (list.empty()) {
+        return os << "{}";
+    }
+
+    os << "{\n";
+
+    for (auto it = list.begin(); it != list.end(); ++it) {
+        os << '\t' << *it;
+
+        if (it != list.end() - 1) {
+            os << ',';
+        }
+
+        os << '\n';
+    }
+
+    os << '}';
+
+    return os;
 }
