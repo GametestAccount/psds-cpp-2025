0aec85efa7b43218fc1be8e53294104dda692201
Ruslan <ruslan@linux-mint.dlink>
2025-12-17 16:08:51 +0500

    add (solution): add range task

--- 03_week/tasks/range/range.cpp
+++ 03_week/tasks/range/range.cpp
@@ -1,7 +1,33 @@
-#include <stdexcept>
 #include <vector>
+#include <cstdlib>
+#include <cmath>
 
+std::vector<int> Range(int from, int to, int step = 1) {
+    std::vector<int> res;
 
-std::vector<int> Range(int from, int to, int step) {
-    throw std::runtime_error{"Not implemented"};
+    if (!step) {
+        return res;
+    }
+
+    if ((from > to) && (step > 0)) {
+        return res;
+    }
+
+    if ((from < to) && (step < 0)) {
+        return res;
+    }
+
+    size_t elements = std::abs(std::ceil((to - from) / (double)step));
+
+    if (!elements) {
+        return res;
+    }
+
+    res.reserve(elements);
+    
+    for (int i = from; (from < to) ? (i < to) : (i > to); i += step) {
+        res.push_back(i);
+    }
+
+    return res;
 }
