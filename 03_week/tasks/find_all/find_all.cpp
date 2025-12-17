c0424e6aee57428db3e6bccb894b91dd7b805a75
Ruslan <ruslan@linux-mint.dlink>
2025-12-17 16:07:13 +0500

    add (solution): add find_all task

--- 03_week/tasks/find_all/find_all.cpp
+++ 03_week/tasks/find_all/find_all.cpp
@@ -1,6 +1,27 @@
-#include <stdexcept>
+#include <vector>
 
+auto FindAll(const std::vector<int>& vec, bool (*f)(int)) {
+    std::vector<size_t> res;
+    
+    if (vec.empty() || !f) {
+        return res;
+    }
 
-/* return_type */ FindAll(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+    size_t count = 0;
+
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (f(vec[i])) {
+            ++count;
+        }
+    }
+
+    res.reserve(count);
+
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (f(vec[i])) {
+            res.push_back(i);
+        }
+    }
+
+    return res;
 }
