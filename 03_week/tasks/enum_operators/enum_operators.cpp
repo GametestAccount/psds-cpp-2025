22bafb4d1397685e0506e54698df04e1b968d99a
Ruslan <ruslan@linux-mint.dlink>
2025-12-17 16:05:56 +0500

    add (solution): add enum_operators task

--- 03_week/tasks/enum_operators/enum_operators.cpp
+++ 03_week/tasks/enum_operators/enum_operators.cpp
@@ -1,5 +1,6 @@
-#include <stdexcept>
+#include <cstdint>
 #include <vector>
+#include <iostream>
 
 enum class CheckFlags : uint8_t {
     NONE = 0,
@@ -12,22 +13,80 @@ enum class CheckFlags : uint8_t {
     ALL = TIME | DATE | USER | CERT | KEYS | DEST
 };
 
-/* return_type */ operator|(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+CheckFlags operator|(const CheckFlags& lhs, const CheckFlags& rhs) {
+    uint8_t lhs_u = static_cast<uint8_t>(lhs);
+    uint8_t rhs_u = static_cast<uint8_t>(rhs);
+    uint8_t all_u = static_cast<uint8_t>(CheckFlags::ALL);
+
+    return static_cast<CheckFlags>((lhs_u | rhs_u) & all_u);
 }
 
-/* return_type */ operator&(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+bool operator&(const CheckFlags& lhs, const CheckFlags& rhs) {
+    uint8_t all_u = static_cast<uint8_t>(CheckFlags::ALL);
+    uint8_t none_u = static_cast<uint8_t>(CheckFlags::NONE);
+
+    uint8_t lhs_u = static_cast<uint8_t>(lhs) & all_u;
+    uint8_t rhs_u = static_cast<uint8_t>(rhs) & all_u;
+    
+    if ((lhs_u == none_u) || (rhs_u == none_u)) {
+        return lhs_u & rhs_u;
+    }
+
+    return ((lhs_u & rhs_u) == rhs_u) || ((rhs_u & lhs_u) == lhs_u);
 }
 
-/* return_type */ operator^(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+CheckFlags operator^(const CheckFlags& lhs, const CheckFlags& rhs) {
+    uint8_t lhs_u = static_cast<uint8_t>(lhs);
+    uint8_t rhs_u = static_cast<uint8_t>(rhs);
+    uint8_t all_u = static_cast<uint8_t>(CheckFlags::ALL);
+
+    return static_cast<CheckFlags>((lhs_u ^ rhs_u) & all_u);
 }
 
-/* return_type */ operator~(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+CheckFlags operator~(const CheckFlags& other) {
+    uint8_t other_u = static_cast<uint8_t>(other);
+    uint8_t all_u = static_cast<uint8_t>(CheckFlags::ALL);
+
+    return static_cast<CheckFlags>((~other_u) & all_u);
 }
 
-/* return_type */ operator<<(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+std::ostream& operator<<(std::ostream& lhs, const CheckFlags& rhs) {
+    uint8_t all_u = static_cast<uint8_t>(CheckFlags::ALL);
+    uint8_t rhs_u = static_cast<uint8_t>(rhs) & all_u;
+
+    if (rhs_u == static_cast<uint8_t>(CheckFlags::NONE)) {
+        return lhs << "NONE";
+    }
+
+    std::string flags = "";
+
+    for (uint8_t i = 1; i < all_u; i <<= 1) {
+        switch (static_cast<CheckFlags>(rhs_u & i)) {
+            case CheckFlags::TIME:
+                flags += "TIME, ";
+                break;
+            case CheckFlags::DATE:
+                flags += "DATE, ";
+                break;
+            case CheckFlags::USER:
+                flags += "USER, ";
+                break;
+            case CheckFlags::CERT:
+                flags += "CERT, ";
+                break;
+            case CheckFlags::KEYS:
+                flags += "KEYS, ";
+                break;
+            case CheckFlags::DEST:
+                flags += "DEST, ";
+                break;
+        }
+    }
+
+    if (!flags.empty()) {
+        flags.pop_back();
+        flags.pop_back();
+    }
+
+    return lhs << flags;
 }
